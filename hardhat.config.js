64adb28c21caf0ea5d71efc581f7f3e29bd9b569
Matias Hernandez <matiasfh@gmail.com>
2022-01-05 11:33:38 -0300

    setup

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  paths: {
+    artifacts: './src/artifacts',
+    sources: './src/contracts'
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337 // To be able to work with metamask
+    }
+  }
+};
