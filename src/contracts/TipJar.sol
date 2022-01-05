4684c43f2bfd06a27768f264dbf4dbc684d57f1e
Matias Hernandez <matiasfh@gmail.com>
2022-01-05 11:35:26 -0300

    init contract

---
+++ src/contracts/TipJar.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4; //same as hardhat.config.js
+
+import 'hardhat/console.sol'; // This allow you to use console.log
+
+contract TipJar {}
