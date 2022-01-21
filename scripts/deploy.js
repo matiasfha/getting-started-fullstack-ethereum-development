5419b8f044f14457fabd0cde7ed96abfcac06a97
Matias Hernandez <matiasfh@gmail.com>
2022-01-21 08:04:25 -0300

    deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+const main = async () => {
+    const [deployer] = await hre.ethers.getSigners();
+    const accountBalance = await deployer.getBalance();
+
+    console.log('Deploying contracts with account: ', deployer.address);
+    console.log('Account balance: ', accountBalance.toString());
+
+    const Token = await hre.ethers.getContractFactory('TipJar');
+    const portal = await Token.deploy();
+    await portal.deployed();
+    console.log('TipJar address: ', portal.address);
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+runMain();
